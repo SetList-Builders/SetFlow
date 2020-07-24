1ec4a0c5de808dcca7120e161600ddcf362a794d
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-07-23 19:27:28 -0500

    Add components & files related to it

---
+++ client/src/MainPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import GigList from './components/GigList/GigList';
+
+class MainPage extends Component {
+  render() {
+    return (
+      <div>
+        <GigList />
+      </div>
+    );
+  }
+}
+
+export default MainPage;
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import MainPage from './MainPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<MainPage />, document.getElementById("root"));
 
