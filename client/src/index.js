ddf619f912ebd862ce78bdb1599f4a4120b386d5
Firecraker <mezamic000@gmail.com>
2020-07-24 12:46:49 -0500

    created basic/minimalist login page

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MainPage from './MainPage';
+import LoginPage from './LoginPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<MainPage />, document.getElementById("root"));
+ReactDOM.render(<LoginPage />, document.getElementById("root"));
 
