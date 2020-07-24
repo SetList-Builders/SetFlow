e63922b51139962331fb89cd7a63ef0c676b5320
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-07-23 20:41:23 -0500

    Import InputSubmit comp

---
+++ client/src/components/GigList/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import InputSubmit from '../InputSubmit';
+
+const GigList = () => {
+  return (
+    <div>
+      Gigs
+      <InputSubmit />
+    </div>
+  );
+};
+
+export default GigList;
---
+++ client/src/components/SetList/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import InputSubmit from '../InputSubmit';
+
+const SetList = () => {
+  return (
+    <div>
+      Setlists
+      <InputSubmit />
+    </div>
+  );
+};
+
+export default SetList;
