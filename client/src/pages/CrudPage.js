c6c8e1016e13bd9a8a1d9604f1ec4ccf6c00d2d5
Firecraker <mezamic000@gmail.com>
2020-08-02 15:51:18 -0500

    passed through props and data

--- client/src/pages/CrudPage.js
+++ client/src/pages/CrudPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Container, Row, Col, Card } from 'reactstrap';
 import NavTop from '../components/NavTop';
 import GigList from "../components/GigList"
@@ -12,50 +12,53 @@ import Loading from "../components/Loading/Loading";
 import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 import Auth0Profile from '../components/Auth0Provider/Auth0Profile';
 
-const CrudPage = () => {
-  const { user } = useAuth0();
 
-  return (
-    <div>
-      <NavTop />
-      
-      <Auth0Profile></Auth0Profile>
-                
-      <Container>
-        <Row>
-          <Col sm="4">
-            <Card body>
-              <GigList />
-              <div className="btn-title">Add Gig:</div>
-              <InputSubmit />
-            </Card>
-          </Col >
-          <Col sm="4">
-            <Card body>
-              <GigSetList />
-            </Card>
-          </Col>
-          <Col sm="4">
-            <Card body>
-              <SavedSets />
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col sm="4">
-            <AllSetList />
-          </Col >
-          <Col sm="4">
-            <Songs />
-          </Col>
-          <Col sm="4">
-            <AddSong />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+const CrudPage = props => {
+    const { user } = useAuth0();
+    console.log(props)
+    return (
+      <div>
+        <NavTop />
+        
+        <Auth0Profile></Auth0Profile>
+                  
+        <Container>
+          <Row>
+            <Col sm="4">
+              <Card body>
+                <GigList />
+                <div className="btn-title">Add Gig:</div>
+                <InputSubmit />
+              </Card>
+            </Col >
+            <Col sm="4">
+              <Card body>
+                {/* <GigSetList /> */}
+              </Card>
+            </Col>
+            <Col sm="4">
+              <Card body>
+                {/* <SavedSets /> */}
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm="4">
+              {/* <AllSetList /> */}
+            </Col >
+            <Col sm="4">
+              <Songs />
+            </Col>
+            <Col sm="4">
+              {/* <AddSong /> */}
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
 }
+
+
 
 export default withAuthenticationRequired(CrudPage, {
   onRedirecting: () => <Loading />,
