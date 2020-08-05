05924aca6e1731aa7d015e7bcb18b4688e62fb0f
Firecraker <mezamic000@gmail.com>
2020-08-04 19:08:23 -0500

    converted crud and launch page to stateful

--- client/src/components/AllSetList/index.js
+++ client/src/components/AllSetList/index.js
@@ -1,32 +1,32 @@
 import React from 'react';
-import InputSubmit from '../InputSubmit';
 import { Card, CardTitle, CardText, } from 'reactstrap';
+import Instructions from "../Instructions"
 
 const AllSetList = props => {
   console.log(props.data.setlists)
   if (props.data.setlists.length === 0) {
     return (
-      <div className="gigCard">
-      <CardTitle>My Gigs</CardTitle>
-      <CardText>
-        <strong>No Setlist have been created. <br /> To Create a Setlist, Go to the Edit Page.</strong>
-      </CardText>
-    </div>
+      <div className="SetlistCard">
+        <CardTitle>My Setlists</CardTitle>
+        <CardText>
+          <strong>No Setlist Added.</strong>
+        </CardText>
+      </div>
     )
   } else {
     return (
       <div className="setlistCard">
-          <CardTitle>Setlists:</CardTitle>
-          <CardText>
-            <ol className="setlist">
-              {props.data.setlists.map(setlist => {
-                return (
-                  <li className="setlistlistItem" id={setlist.id} key={setlist.id}>{setlist.name}
-                  </li>
-                )
-              })}
-            </ol>
-          </CardText>
+        <CardTitle>Setlists:</CardTitle>
+        <CardText>
+          <ol className="setlist">
+            {props.data.setlists.map(setlist => {
+              return (
+                <li className="setlistlistItem" id={setlist.id} key={setlist.id}>{setlist.name}
+                </li>
+              )
+            })}
+          </ol>
+        </CardText>
       </div>
     )
   }
