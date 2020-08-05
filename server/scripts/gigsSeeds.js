f3fd324a0ccaf91a5e693e94b5b6dd70d27270ca
Firecraker <mezamic000@gmail.com>
2020-08-04 20:43:33 -0500

    passed in props for onClick functionality

--- server/scripts/gigsSeeds.js
+++ server/scripts/gigsSeeds.js
@@ -1,30 +1,32 @@
 const mongoose = require("mongoose")
-const db = require ("../models");
+const db = require("../models");
 
 mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/setflow"
+  process.env.MONGODB_URI || "mongodb://localhost/setflow"
 )
 
-async function findUserForGig(){
-    const user = await db.User.findOne({username:"User1"})
-    const setlists = await db.Setlist.find({})
-    const gigSeed = [
-        {
-            user: user,
-            name: "Gig 1",
-            setlists: setlists
-        }
-    ]
-    db.Gig
-  .deleteMany({})
-  .then(() => db.Gig.create(gigSeed)
-  .then(data => {
-      console.log("records inserted!")
-  })
-  .catch(err => {
-      console.log(err)
-      process.exit(1)
-  }))
+async function findUserForGig() {
+  const user = await db.User.findOne({ username: "User1" })
+  const setlists = await db.Setlist.find({})
+  const gigSeed = [
+    {
+      user: user,
+      name: "Gig 1",
+      setlists: setlists
+    }
+  ]
+  db.Gig
+    .deleteMany({})
+    .then(() => db.Gig.create(gigSeed)
+      .then(data => {
+        console.log("records inserted!")
+      })
+      .catch(err => {
+        console.log(err)
+        process.exit(1)
+      }))
 }
 
-module.exports = {findUserForGig}
+findUserForGig()
+
+// module.exports = { findUserForGig }
