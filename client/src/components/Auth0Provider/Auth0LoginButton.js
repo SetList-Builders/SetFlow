f7babc7afdfaca9f6c8878785122307d3e18314a
Firecraker <mezamic000@gmail.com>
2020-08-05 20:37:56 -0500

    main page css

--- client/src/components/Auth0Provider/Auth0LoginButton.js
+++ client/src/components/Auth0Provider/Auth0LoginButton.js
@@ -4,7 +4,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 const Auth0LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Auth0 Log In</button>;
+  return <button style={{ backgroundColor: "#080939", border: "solid 1px #cea935", color: "#cea935", fontSize: "22px", borderRadius: "4px" }} onClick={() => loginWithRedirect()}>Login</button>;
 };
 
 export default Auth0LoginButton;
