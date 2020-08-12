ea02fb5f433ef043a515c1de188535246715be4d
Drake24 <zjhassler@gmail.com>
2020-08-12 17:05:35 -0500

    working on heroku deploy in servejs

---
+++ client/build/precache-manifest.ac065d70ac820141d7f687ccb5f1aa24.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54df4b6a18cb93c699fb5710c41d3a02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2161134890af870a831a",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "53f903563a6851ac731d",
+    "url": "/static/css/main.9cdc053b.chunk.css"
+  },
+  {
+    "revision": "2161134890af870a831a",
+    "url": "/static/js/2.04cebd68.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.04cebd68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53f903563a6851ac731d",
+    "url": "/static/js/main.fea833e7.chunk.js"
+  },
+  {
+    "revision": "6dd5eaa6cc26056a146f",
+    "url": "/static/js/runtime-main.5b3571be.js"
+  },
+  {
+    "revision": "b7d30102ca8d71ba5f2a6bd75e3acf37",
+    "url": "/static/media/final_5f2acf95c3990e001356f1ed_198465.b7d30102.gif"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "96943f7bd1295d66ba325a6dfd0a9521",
+    "url": "/static/media/logo-icon.96943f7b.png"
+  },
+  {
+    "revision": "f3d94296c3317a39f0c05c4c9120d149",
+    "url": "/static/media/logo-text.f3d94296.png"
+  },
+  {
+    "revision": "c31ae9c2184f595829fdcc1fc156b937",
+    "url": "/static/media/logo.c31ae9c2.png"
+  }
+]);
