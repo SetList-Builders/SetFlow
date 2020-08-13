bf2595c0a23bd8f36570faab1c6f5cc721edc601
Drake24 <zjhassler@gmail.com>
2020-08-13 17:11:55 -0500

    still trying to get redirect to work

--- client/src/index.js
+++ client/src/index.js
@@ -23,8 +23,8 @@ ReactDOM.render(
       domain={config.domain}
       clientId={config.clientId}
       audience={config.audience}
-      redirectUri={window.location.origin,"/main"}
-      onRedirectCallback={onRedirectCallback + "/main"}
+      redirectUri={window.location.origin + "/main"}
+      onRedirectCallback={onRedirectCallback}
     >
       <App />
     </Auth0Provider>
